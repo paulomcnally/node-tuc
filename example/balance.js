ef7803b86c51927017f4bc31d0fa8fd28254d460
Paulo McNally <paulomcnally@gmail.com>
2014-08-14 22:43:39 -0600

    added debuggable metthod

--- example/balance.js
+++ example/balance.js
@@ -3,7 +3,9 @@ var Tuc = require('../lib/tuc');
 // instance class
 var tuc = new Tuc();
 // set number
-var number = '00759794';
+var number = '00007564';
+// enable debug;
+tuc.setDebug(false);
 // call method getBalance
 tuc.getBalance(number, function(balance) {
   console.log(balance);
--- example/type.js
+++ example/type.js
@@ -4,6 +4,8 @@ var Tuc = require('../lib/tuc');
 var tuc = new Tuc();
 // set number
 var number = '00759794';
+// enable debug;
+tuc.setDebug(false);
 // call method getType
 tuc.getType(number, function(balance) {
   console.log(balance);
