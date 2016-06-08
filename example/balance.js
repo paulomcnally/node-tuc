e10b853bd962a5a65b267d51e5e4fe41ebdf9f85
Paulo McNally <paulomcnally@gmail.com>
2016-06-08 02:11:45 -0600

    remove debug

--- example/balance.js
+++ example/balance.js
@@ -7,9 +7,6 @@ const tuc = new Tuc();
 // set number
 let number = '00758888';
 
-// enable debug;
-tuc.setDebug(false);
-
 // call method getBalance
 tuc.getBalance(number, (balance) => {
   console.log(balance);
--- example/type.js
+++ example/type.js
@@ -7,9 +7,6 @@ const tuc = new Tuc();
 // set number
 let number = '00759794';
 
-// enable debug;
-tuc.setDebug(false);
-
 // call method getType
 tuc.getType(number, (balance) => {
   console.log(balance);
