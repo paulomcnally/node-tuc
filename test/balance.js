e68ae3c5b0c794203c733fcd371593145224df17
Hosmel Quintana <hosmelq@gmail.com>
2017-03-02 19:32:54 -0600

    Refactor code

--- test/balance.js
+++ test/balance.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
 const is = require('is_js');
 const Tuc = require('../lib/tuc');
+
 const tuc = new Tuc();
+
 describe('Tuc', () => {
   describe('getBalance', () => {
-
     it('VALID', (done) => {
       tuc.getBalance('00759799', (response) => {
         assert(is.number(parseFloat(response)));
